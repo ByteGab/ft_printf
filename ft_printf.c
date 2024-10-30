5785ffd26f37874fba27e3682f05372076ca31ca
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-30 17:15:04 +0100

    change ft_printfç

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:08:26 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/30 16:20:44 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/30 17:00:33 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,29 +23,29 @@
 
 int	ft_printf(char const *format, ...)
 {
-    va_list arguments;
-    int i;
-    
-    va_start(arguments,format);
-    i = 0;
-    while (format[i] != NULL)
-    {
-        if(format[i] == '%')
-        {
-            if(format[i + 1] != '\0')
-            {
-                formats(format,arguments);   
-            }
-        }
-        i++;
-    }
-    
+	va_list	arguments;
+	int		i;
+
+	va_start(arguments, format);
+	i = 0;
+	while (format[i] != NULL)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+			{
+				formats(format, arguments);
+			}
+		}
+		i++;
+	}
 	return (0);
 }
-static char formats (arguments,format)
+static char	formats(va_list arguments, char format)
 {
-    if (format == 'd')
-    {
-        ft_putnbr_fd(va_arg(arguments,int));
-    }
+	if (format == 'd')
+	{
+		ft_putnbr_fd(va_arg(arguments, int), 1);
+		// llamar itoa para saber tamaño
+	}
 }
