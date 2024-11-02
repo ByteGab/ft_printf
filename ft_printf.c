cfd7a6f5fd4bb4cb5e08e6b9fa24a37273717e1e
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-02 20:37:55 +0100

    delete libft

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,10 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:08:26 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/02 20:17:45 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/02 20:37:30 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "libftprintf.h"
 #include <stdarg.h>
 
