7fbaca9f9053632eaa92dfb327fac6e4e593d5ac
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-02 20:24:44 +0100

    add libftprintf

---
+++ libftprintf.h
@@ -0,0 +1,10 @@
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+int	ft_printf(char const *format, ...);
+int	ft_putcharInt(char c);
+int	ft_putstrInt(char *s);
+int	ft_putnbrInt(int n);
+int	ft_putnbrUn(unsigned int n);
+
+#endif
