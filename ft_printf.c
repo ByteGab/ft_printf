0b2ddfffd11bf6faf5f9a40200e7f3fb9ff09a98
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-02 20:22:09 +0100

    change function formats

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:08:26 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/01 18:31:17 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/02 20:17:45 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include "libftprintf.h"
 #include <stdarg.h>
 
 // va_start
@@ -21,70 +22,76 @@
 // malloc
 // free
 
-int	ft_printf(char const *format, ...)
-{
-	va_list	arguments;
-	int		i;
-	int		value;
-
-	va_start(arguments, format);
-	i = 0;
-	value = 0;
-	while (format[i] != NULL)
-	{
-		write(1, format[i], 1);
-		if (format[i] == '%')
-		{
-			if (format[i + 1] != '\0')
-			{
-				value += formats(arguments, format[i]);
-			}
-		}
-		i++;
-	}
-	value += i;
-	return (value);
-}
 static int	formats(va_list arguments, char format)
 {
-	int size_format;
+	int	size_format;
 
 	size_format = 0;
 	if (format == 'c')
-	{
-		ft_putchar_fd(va_arg(arguments, char), 1);
-	}
+		size_format = ft_putcharInt(va_arg(arguments, char));
 	else if (format == 's')
-	{
-		ft_putstr_fd(va_arg(arguments, char *), 1);
-		// falta añadir \0
-	}
+		size_format = ft_putstrInt(va_arg(arguments, char *));
 	else if (format == 'p')
 	{
+		// El puntero void* dado como argumento se imprime en formato hexadecimal
 	}
 	else if (format == 'd')
-	{
-		ft_putnbr_fd(va_arg(arguments, int), 1);
-		// size_format = ();
-		// llamar itoa para saber tamaño
-	}
+		size_format = ft_putnbrInt(va_arg(arguments, int));
 	else if (format == 'i')
-	{
-	}
+		size_format = ft_putnbrInt(va_arg(arguments, int));
 	else if (format == 'u')
 	{
+		// Imprime un número decimal (base 10) sin signo.
 	}
 	else if (format == 'x')
 	{
+		// Imprime un número hexadecimal (base 16) en minúsculas
 	}
 	else if (format == 'X')
 	{
+		// Imprime un número hexadecimal (base 16) en mayúsculas.
 	}
 	else if (format == '%')
 	{
 		write(1, '%', 1);
 		size_format = 1;
 	}
-
 	return (size_format);
+}
+
+int	ft_printf(char const *format, ...)
+{
+	int		i;
+	int		value;
+	va_list	arguments;
+
+	i = 0;
+	value = 0;
+	va_start(arguments, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+			{
+				value += formats(arguments, format[i + 1]);
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+		i++;
+	}
+	va_end(arguments);
+	value += i;
+	return (value);
+}
+
+int	main(void)
+{
+	int print = ft_printf("Hola que tal %s", "hola");
+	write(1, "\n", 1);
+	ft_printf("%i", print);
+	write(1, "\n", 1);
 }
