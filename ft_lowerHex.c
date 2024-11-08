7a6252bfe0b6cd89e11c6d5ff9cc904a1d784283
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-08 20:10:24 +0100

    change variable

--- ft_lowerHex.c
+++ ft_lowerHex.c
@@ -6,11 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:07:02 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/08 19:10:15 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/08 19:55:37 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_lowerHex(char *s)
+int	ft_lowerHex(int n)
 {
+    int size_format;
     
+    size_format = 0;
+	if (n > 16)
+	{
+		size_format += ft_lowerHex(n / 10);
+		size_format += ft_lowerHex(n % 10);
+	}
 }
