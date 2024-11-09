91830b43773522f3b0e20c73e286dbe3fc20eff6
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 20:00:56 +0100

    chnage ft_voidHex

--- ft_voidHex.c
+++ ft_voidHex.c
@@ -6,34 +6,59 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:22:57 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/08 19:23:40 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:51:54 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_voidHex(void *n)
+int	ft_strlenHex(unsigned long long n)
 {
-	unsigned long long nL;
-    int size_format;
-	char * baseHex;
-    
-    size_format = 0;
-	nL = (unsigned long long)n;
-	baseHex = "0123456789abcdef";
-	if (nL == 0)
+	int	size_format;
+
+	size_format = 0;
+	if (n <= 0)
+		size_format++;
+	while (n)
 	{
-		size_format += ft_putstrInt("0x0");
-		return (size_format);
+		n /= 16;
+		size_format++;
 	}
-    size_format += ft_putstrInt("0x");
-	if (nL >= 16)
+	return (size_format);
+}
+
+void	ft_putHex(unsigned long long n)
+{
+	char	*baseHex;
+
+	baseHex = "0123456789abcdef";
+	if (n >= 16)
 	{
-		size_format += ft_voidHex((void *)(nL/ 16));
+		ft_putHex(n / 16);
+		ft_putHex(n % 16);
 	}
 	else
 	{
-		size_format += ft_putcharInt((unsigned int)baseHex[nL % 16]);
+			ft_putcharInt((unsigned int)baseHex[n % 16]);	
+	}
+}
+
+int	ft_voidHex(void *n)
+{
+	unsigned long long nL;
+	int size_format;
+
+	size_format = 0;
+	nL = (unsigned long long)n;
+	size_format += ft_putstrInt("0x");
+	if (nL == 0)
+	{
+		size_format += ft_putstrInt("0");
+	}
+	else
+	{
+		ft_putHex(nL);
+		size_format += ft_strlenHex(nL);
 	}
 	return (size_format);
 }
