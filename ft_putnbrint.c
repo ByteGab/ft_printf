ae7c9852444af1beff2d147d4bf89dde092d6041
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 20:52:44 +0100

    create ft_putnbrint

---
+++ ft_putnbrint.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbrint.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-09 19:35:25 by gafreire          #+#    #+#             */
+/*   Updated: 2024-11-09 19:35:25 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbrint(int n)
+{
+	int	size_format;
+
+	size_format = 0;
+	if (n == -2147483648)
+	{
+		return (write(1, "-2147483648", 11));
+	}
+	if (n < 0)
+	{
+		size_format += ft_putcharint('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		size_format += ft_putnbrint(n / 10);
+		size_format += ft_putnbrint(n % 10);
+	}
+	else
+	{
+		size_format += ft_putcharint(n + '0');
+	}
+	return (size_format);
+}
