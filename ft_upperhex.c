84017621c544646c9858edb467a1361bb0276982
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 20:54:10 +0100

    create ft_upperhex

---
+++ ft_upperhex.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_upperhex.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-09 19:41:24 by gafreire          #+#    #+#             */
+/*   Updated: 2024-11-09 19:41:24 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_upperhex(unsigned int n)
+{
+	int		size_format;
+	char	*basehex;
+
+	size_format = 0;
+	basehex = "0123456789ABCDEF";
+	if (n >= 16)
+	{
+		size_format += ft_upperhex(n / 16);
+		size_format += ft_upperhex(n % 16);
+	}
+	else
+	{
+		size_format += ft_putcharint(basehex[n % 16]);
+	}
+	return (size_format);
+}
