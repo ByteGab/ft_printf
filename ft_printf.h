358df457cff31ce987476aba13e09659c571a88b
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 19:58:42 +0100

    change ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:14:17 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/08 20:02:41 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:17:28 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
+
 int	ft_printf(char const *format, ...);
 int	ft_putcharInt(int c);
 int	ft_putstrInt(char *s);
-int ft_voidHex(void *n);
+int	ft_voidHex(void *n);
 int	ft_putnbrInt(int n);
 int	ft_putnbrUn(unsigned int n);
 int	ft_upperHex(unsigned int n);
 int	ft_lowerHex(unsigned int n);
-
 
 #endif
