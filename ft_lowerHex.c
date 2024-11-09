2b9da8f780394837f201762f52b8736a701eb549
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 19:57:48 +0100

    change ft_lowerHex

--- ft_lowerHex.c
+++ ft_lowerHex.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:07:02 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/08 19:55:37 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:13:46 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	ft_lowerHex(unsigned int n)
 {
-    int size_format;
-	char * baseHex;
-    
-    size_format = 0;
+	int size_format;
+	char *baseHex;
+
+	size_format = 0;
 	baseHex = "0123456789abcdef";
 	if (n >= 16)
 	{
