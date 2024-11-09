8311489dfd663a3b06089029a28a8749595be387
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-09 20:00:27 +0100

    change ft_upperHex

--- ft_upperHex.c
+++ ft_upperHex.c
@@ -6,18 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:08:02 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/08 19:57:56 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:13:39 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_printf.h"
+#include "ft_printf.h"
 
-int ft_upperHex(unsigned int n)
-{   
-    int size_format;
-	char * baseHex;
-    
-    size_format = 0;
+int	ft_upperHex(unsigned int n)
+{
+	int size_format;
+	char *baseHex;
+
+	size_format = 0;
 	baseHex = "0123456789ABCDEF";
 	if (n >= 16)
 	{
