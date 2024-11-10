38b728c5fd6e64d354f47e6d2bebf151b33941bb
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-10 17:58:43 +0100

    change ft_putnbrun

--- ft_putnbrun.c
+++ ft_putnbrun.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbrun.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-11-09 19:33:34 by gafreire          #+#    #+#             */
-/*   Updated: 2024-11-09 19:33:34 by gafreire         ###   ########.fr       */
+/*   Created: 2024/11/09 19:33:34 by gafreire          #+#    #+#             */
+/*   Updated: 2024/11/10 16:56:12 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	ft_putnbrun(unsigned int n)
 		size_format += ft_putnbrint(n % 10);
 	}
 	else
-	{
 		size_format += ft_putcharint(n + '0');
-	}
 	return (size_format);
 }
