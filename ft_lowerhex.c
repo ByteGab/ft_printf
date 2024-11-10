c472b0b5b924a7ec2150ce04dea878e25a5de08f
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-10 17:57:27 +0100

    change ft_lowerhex

--- ft_lowerhex.c
+++ ft_lowerhex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lowerhex.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-11-09 19:37:31 by gafreire          #+#    #+#             */
-/*   Updated: 2024-11-09 19:37:31 by gafreire         ###   ########.fr       */
+/*   Created: 2024/11/09 19:37:31 by gafreire          #+#    #+#             */
+/*   Updated: 2024/11/10 16:55:12 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ int	ft_lowerhex(unsigned int n)
 	basehex = "0123456789abcdef";
 	if (n >= 16)
 	{
-		size_format += ft_lowerhex((unsigned int)n / 16);
-		size_format += ft_lowerhex((unsigned int)n % 16);
+		size_format += ft_lowerhex(n / 16);
+		size_format += ft_lowerhex(n % 16);
 	}
 	else
-	{
 		size_format += ft_putcharint(basehex[n % 16]);
-	}
 	return (size_format);
 }
