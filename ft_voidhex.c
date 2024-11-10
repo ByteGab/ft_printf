a80bf51fa28be9df6a2b84029c05d519c3a6250a
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-10 17:59:32 +0100

    change ft_voidhex

--- ft_voidhex.c
+++ ft_voidhex.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_voidhex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-11-09 19:41:29 by gafreire          #+#    #+#             */
-/*   Updated: 2024-11-09 19:41:29 by gafreire         ###   ########.fr       */
+/*   Created: 2024/11/09 19:41:29 by gafreire          #+#    #+#             */
+/*   Updated: 2024/11/10 17:46:09 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_strlenhex(unsigned long long n)
+static int	strlenhex(unsigned long long n)
 {
 	int	size_format;
 
@@ -27,20 +27,18 @@ static int	ft_strlenhex(unsigned long long n)
 	return (size_format);
 }
 
-static void	ft_puthex(unsigned long long n)
+static void	puthex(unsigned long long n)
 {
 	char	*base_hex;
 
 	base_hex = "0123456789abcdef";
 	if (n >= 16)
 	{
-		ft_puthex(n / 16);
-		ft_puthex(n % 16);
+		puthex(n / 16);
+		puthex(n % 16);
 	}
 	else
-	{
 		ft_putcharint((unsigned int)base_hex[n % 16]);
-	}
 }
 
 int	ft_voidhex(void *n)
@@ -49,16 +47,14 @@ int	ft_voidhex(void *n)
 	unsigned long long	nl;
 
 	size_format = 0;
+	if (n == NULL)
+	{
+		size_format += ft_putstrint("(nil)");
+		return (size_format);
+	}
 	nl = (unsigned long long)n;
 	size_format += ft_putstrint("0x");
-	if (nl == 0)
-	{
-		size_format += ft_putstrint("0");
-	}
-	else
-	{
-		ft_puthex(nl);
-		size_format += ft_strlenhex(nl);
-	}
+	puthex(nl);
+	size_format += strlenhex(nl);
 	return (size_format);
 }
